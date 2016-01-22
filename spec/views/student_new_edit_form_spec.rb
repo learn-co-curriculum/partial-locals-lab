4339e5958d68299f2c699a171fde47bb0febdbe0
Blake Johnson <blake41@gmail.com>
2016-01-22 15:53:04 -0500

    had to do some weird changes to the tests to get them to work correctly

--- spec/views/student_new_edit_form_spec.rb
+++ spec/views/student_new_edit_form_spec.rb
@@ -1,60 +1,58 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "create and edit form" do
+RSpec.describe 'create and edit form' do
   describe 'create form' do
-    it "renders create student form from the new.html file" do
+    it 'renders create student form from the new.html file' do
       view.lookup_context.prefixes = %w[students]
       student = Student.new
       assign(:student, student)
-      render :template => "students/new.html.erb"
+      render template: 'students/new.html.erb'
       expect(rendered).to match /Create Student/
     end
 
-    it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
+    it 'directly renders the form in a students/form partial with the form object passed through as a local variable' do
       view.lookup_context.prefixes = %w[students]
       student = Student.new
       assign(:student, student)
-      render :template => "students/new.html.erb"
-      expect(rendered).to render_template(:partial => "_form")
+      render template: 'students/new.html.erb'
+      expect(rendered).to render_template(partial: '_form')
     end
 
-
-    it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
+    it 'directly renders the form in a students/form partial with the form object passed through as a local variable' do
       view.lookup_context.prefixes = %w[students]
       student = Student.new
       assign(:student, student)
 
-      render :template => "students/new.html.erb"
-      expect(rendered).to render_template(:partial => "_form", locals: {student: student})
+      render template: 'students/new.html.erb'
+      expect(rendered).to render_template(partial: '_form', locals: { student: student })
     end
   end
 
   describe 'edit form' do
-    it "renders edit student form from the edit.html file" do
+    it 'renders edit student form from the edit.html file' do
       view.lookup_context.prefixes = %w[students]
       student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
       assign(:student, student)
-      render :template => "students/edit.html.erb"
+      render template: 'students/edit.html.erb'
       expect(rendered).to match /Update Student/
     end
 
-    it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
+    it 'directly renders the form in a students/form partial with the form object passed through as a local variable' do
       view.lookup_context.prefixes = %w[students]
       student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
       assign(:student, student)
-      render :template => "students/new.html.erb"
+      render template: 'students/new.html.erb'
 
-      expect(rendered).to render_template(:partial => "_form")
+      expect(rendered).to render_template(partial: '_form')
     end
 
-
-    it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
+    it 'directly renders the form in a students/form partial with the form object passed through as a local variable' do
       view.lookup_context.prefixes = %w[students]
       student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
       assign(:student, student)
 
-      render :template => "students/edit.html.erb"
-      expect(rendered).to render_template(:partial => "_form", locals: {student: student})
+      render template: 'students/edit.html.erb'
+      expect(rendered).to render_template(partial: '_form', locals: { student: student })
     end
   end
 end
