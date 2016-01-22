4bb1ad397b591e31a932e1162d9c43f46a8e7fee
Blake Johnson <blake41@gmail.com>
2016-01-22 15:53:04 -0500

    not sure

--- spec/views/classroom_show_spec.rb
+++ spec/views/classroom_show_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "classroom_show_view" do
   end
 
   it "renders classroom information on the show view" do
-    view.lookup_context.prefixes = %w[students]
+    view.lookup_context.prefixes = %w[students, classrooms]
     student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
     assign(:classroom, classroom)
     render :template => "classrooms/show.html.erb"
@@ -29,14 +29,14 @@ RSpec.describe "classroom_show_view" do
   end
 
   it "renders a students/student partial" do
-    view.lookup_context.prefixes = %w[students]
+    view.lookup_context.prefixes = %w[students, classrooms]
     assign(:classroom, classroom)
     render :template => "classrooms/show.html.erb"
     expect(rendered).to render_template(:partial => "students/_student")
   end
 
   it "displays the student information from the partial" do
-    view.lookup_context.prefixes = %w[students]
+    view.lookup_context.prefixes = %w[students, classrooms]
     assign(:classroom, classroom)
     render :template => "classrooms/show.html.erb"
     expect(classroom.students.count).to eq 5
