438399aafcafbd2f4e0115a70d182503807e3b69
Shami <shamitomita@gmail.com>
2021-10-03 18:44:56 -0400

    commit tree 850c6d5a352be118f1c7fd3bb92ac7267e34ce31

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(query)
+    if query.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end 
 end
